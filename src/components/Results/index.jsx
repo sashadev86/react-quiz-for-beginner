98afc2a77fe9d8435ab3281120fae804298fb2c9
Alexsandr Valentinov <djvalentinoff@gmail.com>
2024-03-16 14:46:54 +0300

    docs: add readme.md

--- src/components/Results/index.jsx
+++ src/components/Results/index.jsx
@@ -5,7 +5,7 @@ const Result = ({ correct, questions }) => {
       <h2>
         Вы отгадали {correct} ответа из {questions.length}
       </h2>
-      <a href='/'>
+      <a href='/react-quiz-for-beginner'>
         <button>Попробовать снова</button>
       </a>
     </div>
